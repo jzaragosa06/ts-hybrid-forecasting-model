56f58f8f7f879ebcf47bd05d20950c8d01e6bb11
Jun Jun M. Zaragosa <144114300+jzaragosa06@users.noreply.github.com>
2024-10-09 01:50:49 +0000

    Result of hybrid model for ridge, rf, and xgboost, where the latter is the meta model.

--- index.py
+++ index.py
@@ -1,24 +1,57 @@
 # import os
 # import pandas as pd
 # import numpy as np
+# import csv
+# import shutil
 # from utility.date_functions import infer_frequency, create_time_features
 # from hybridModels.xgb_rf import evaluate_xgboost_and_random_forest
 # from models.xgboost import forecast_and_evaluate_xgboost
 # from models.random_forest import forecast_and_evaluate_random_forest
+
+
+# # This creates the folder where we can store the evaluation results.
+# #  It rewrites the previous execution's results.
+# folder_name = 'evaluations/xgb_rf'
+
+# if os.path.exists(folder_name):
+#     shutil.rmtree(folder_name)
+#     print("folder deleted")
+
+# os.makedirs(folder_name)
+# print("Folder created")
+
+# csv_files = [
+#     ('mae.csv', ['fname', 'xgb', 'rf', 'xgb_rf']),
+#     ('mape.csv', ['fname', 'xgb', 'rf', 'xgb_rf']),
+#     ('mse.csv',['fname', 'xgb', 'rf', 'xgb_rf']),
+#     ('rmse.csv', ['fname', 'xgb', 'rf', 'xgb_rf'])
+# ]
+
+
+# for filename, header in csv_files:
+#     filepath = os.path.join(folder_name, filename)
+#     with open(filepath, mode = 'w', newline='') as file:
+#         writer = csv.writer(file)
+#         writer.writerow(header)
+#         print(f"{filename} created")
+
 
 # directory = "datasets"
 
 # for filename in os.listdir(directory):
 #     file_path = os.path.join(directory, filename)
 #     df = pd.read_csv(file_path, index_col=0, parse_dates=True)
-
+#     # --------------------------------------------------------
 #     freq = infer_frequency(df)
 #     exog = create_time_features(df=df, freq=freq)
 #     lags = 7
 
+#     print(f"freq on {filename}: {freq}")
+#     print(f"exog on {filename} : {exog}")
+#     # --------------------------------------------------------
 #     results_rf = forecast_and_evaluate_random_forest(df_arg=df, exog=exog, lag_value=lags)
 #     results_xgb = forecast_and_evaluate_xgboost(df_arg=df, exog=exog, lag_value=lags)
-#     results_xgb_rf = evaluate_xgboost_and_random_forest(df_arg=df, exog=exog, lag_value=lags)
+#     hybrid = evaluate_xgboost_and_random_forest(df_arg=df, exog=exog, lag_value=lags)
 
 #     csv_mae = 'evaluations/xgb_rf/mae.csv'
 #     csv_mape = 'evaluations/xgb_rf/mape.csv'
@@ -26,10 +59,10 @@
 #     csv_rmse = 'evaluations/xgb_rf/rmse.csv'
 
 
-#     new_row_mae = pd.DataFrame([[filename, results_xgb['mae'], results_rf['mae'], results_xgb_rf['mae']]], columns=['fname','xgb', 'rf','xgb_rf'])
-#     new_row_mape = pd.DataFrame([[filename, results_xgb['mape'], results_rf['mape'], results_xgb_rf['mape']]], columns=['fname','xgb', 'rf','xgb_rf'])
-#     new_row_mse = pd.DataFrame([[filename, results_xgb['mse'], results_rf['mse'], results_xgb_rf['mse']]], columns=['fname','xgb', 'rf','xgb_rf'])
-#     new_row_rmse = pd.DataFrame([[filename, results_xgb['rmse'], results_rf['rmse'], results_xgb_rf['rmse']]], columns=['fname','xgb', 'rf','xgb_rf'])
+#     new_row_mae = pd.DataFrame([[filename, results_xgb['mae'], results_rf['mae'], hybrid['mae']]], columns=['fname','xgb', 'rf','xgb_rf'])
+#     new_row_mape = pd.DataFrame([[filename, results_xgb['mape'], results_rf['mape'], hybrid['mape']]], columns=['fname','xgb', 'rf','xgb_rf'])
+#     new_row_mse = pd.DataFrame([[filename, results_xgb['mse'], results_rf['mse'], hybrid['mse']]], columns=['fname','xgb', 'rf','xgb_rf'])
+#     new_row_rmse = pd.DataFrame([[filename, results_xgb['rmse'], results_rf['rmse'], hybrid['rmse']]], columns=['fname','xgb', 'rf','xgb_rf'])
 
 
 #     new_row_mae.to_csv(csv_mae,  mode='a', header=False, index=False, lineterminator="\n")
@@ -38,15 +71,44 @@
 #     new_row_rmse.to_csv(csv_rmse, mode='a', header=False, index=False, lineterminator="\n")
 
 
-
-
 import os
 import pandas as pd
 import numpy as np
+import csv
+import shutil
 from utility.date_functions import infer_frequency, create_time_features
 from hybridModels.xgb_rf import evaluate_xgboost_and_random_forest
 from models.xgboost import forecast_and_evaluate_xgboost
 from models.random_forest import forecast_and_evaluate_random_forest
+from models.ridge import forecast_and_evaluate_ridge
+from hybridModels.xgb_rf_ridge import evaluate_xgboost_and_random_forest_ridge
+
+
+# This creates the folder where we can store the evaluation results.
+#  It rewrites the previous execution's results.
+folder_name = "evaluations/xgb_rf_ridge"
+
+if os.path.exists(folder_name):
+    shutil.rmtree(folder_name)
+    print("folder deleted")
+
+os.makedirs(folder_name)
+print("Folder created")
+
+csv_files = [
+    ("mae.csv", ["fname", "xgb", "rf", "ridge", "xgb_rf_ridge"]),
+    ("mape.csv", ["fname", "xgb", "rf", "ridge", "xgb_rf_ridge"]),
+    ("mse.csv", ["fname", "xgb", "rf", "ridge", "xgb_rf_ridge"]),
+    ("rmse.csv", ["fname", "xgb", "rf", "ridge", "xgb_rf_ridge"]),
+]
+
+for filename, header in csv_files:
+    filepath = os.path.join(folder_name, filename)
+    with open(filepath, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        print(f"{filename} created")
+
 
 directory = "datasets"
 
@@ -54,60 +116,86 @@ for filename in os.listdir(directory):
     file_path = os.path.join(directory, filename)
     df = pd.read_csv(file_path, index_col=0, parse_dates=True)
     # --------------------------------------------------------
-    freq = infer_frequency(df)
+    # freq = infer_frequency(df)
+    freq = 'D'
     exog = create_time_features(df=df, freq=freq)
     lags = 7
 
     print(f"freq on {filename}: {freq}")
     print(f"exog on {filename} : {exog}")
     # --------------------------------------------------------
-    results_rf = forecast_and_evaluate_random_forest(df_arg=df, exog=exog, lag_value=lags)
+    results_rf = forecast_and_evaluate_random_forest(
+        df_arg=df, exog=exog, lag_value=lags
+    )
     results_xgb = forecast_and_evaluate_xgboost(df_arg=df, exog=exog, lag_value=lags)
-    results_xgb_rf = evaluate_xgboost_and_random_forest(df_arg=df, exog=exog, lag_value=lags)
+    results_ridge = forecast_and_evaluate_ridge(df_arg=df, exog=exog, lag_value=lags)
+    hybrid = evaluate_xgboost_and_random_forest_ridge(
+        df_arg=df, exog=exog, lag_value=lags
+    )
 
-    csv_mae = 'evaluations/xgb_rf/mae.csv'
-    csv_mape = 'evaluations/xgb_rf/mape.csv'
-    csv_mse = 'evaluations/xgb_rf/mse.csv'
-    csv_rmse = 'evaluations/xgb_rf/rmse.csv'
+    csv_mae = "evaluations/xgb_rf_ridge/mae.csv"
+    csv_mape = "evaluations/xgb_rf_ridge/mape.csv"
+    csv_mse = "evaluations/xgb_rf_ridge/mse.csv"
+    csv_rmse = "evaluations/xgb_rf_ridge/rmse.csv"
 
+    new_row_mae = pd.DataFrame(
+        [
+            [
+                filename,
+                results_xgb["mae"],
+                results_rf["mae"],
+                results_ridge["mae"],
+                hybrid["mae"],
+            ]
+        ],
+        columns=["fname", "xgb", "rf", "ridge", "xgb_rf_ridge"],
+    )
+    new_row_mape = pd.DataFrame(
+        [
+            [
+                filename,
+                results_xgb["mape"],
+                results_rf["mape"],
+                results_ridge["mae"],
+                hybrid["mape"],
+            ]
+        ],
+        columns=["fname", "xgb", "rf", "ridge", "xgb_rf_ridge"],
+    )
+    new_row_mse = pd.DataFrame(
+        [
+            [
+                filename,
+                results_xgb["mse"],
+                results_rf["mse"],
+                results_ridge["mae"],
+                hybrid["mse"],
+            ]
+        ],
+        columns=["fname", "xgb", "rf", "ridge", "xgb_rf_ridge"],
+    )
+    new_row_rmse = pd.DataFrame(
+        [
+            [
+                filename,
+                results_xgb["rmse"],
+                results_rf["rmse"],
+                results_ridge["mae"],
+                hybrid["rmse"],
+            ]
+        ],
+        columns=["fname", "xgb", "rf", "ridge", "xgb_rf_ridge"],
+    )
 
-    new_row_mae = pd.DataFrame([[filename, results_xgb['mae'], results_rf['mae'], results_xgb_rf['mae']]], columns=['fname','xgb', 'rf','xgb_rf'])
-    new_row_mape = pd.DataFrame([[filename, results_xgb['mape'], results_rf['mape'], results_xgb_rf['mape']]], columns=['fname','xgb', 'rf','xgb_rf'])
-    new_row_mse = pd.DataFrame([[filename, results_xgb['mse'], results_rf['mse'], results_xgb_rf['mse']]], columns=['fname','xgb', 'rf','xgb_rf'])
-    new_row_rmse = pd.DataFrame([[filename, results_xgb['rmse'], results_rf['rmse'], results_xgb_rf['rmse']]], columns=['fname','xgb', 'rf','xgb_rf'])
-
-
-    new_row_mae.to_csv(csv_mae,  mode='a', header=False, index=False, lineterminator="\n")
-    new_row_mape.to_csv(csv_mape, mode='a', header=False, index=False, lineterminator="\n")
-    new_row_mse.to_csv(csv_mse, mode='a', header=False, index=False, lineterminator="\n")
-    new_row_rmse.to_csv(csv_rmse, mode='a', header=False, index=False, lineterminator="\n")
-
-
-
-
-
-
-
-
-# # Load the dataset
-# df = pd.read_csv("datasets/candy_production.csv", index_col=0, parse_dates=True)
-
-# # Infer frequency and create exogenous variables (features)
-# freq = infer_frequency(df)
-# print(f"Inferred frequency: {freq}")
-# exog = create_time_features(df_arg=df, freq=freq)
-
-# # Evaluate the model
-# results = evaluate_xgboost_and_random_forest(df=df, exog=exog, lag_value=15)
-# print(f"The MSE of the model is: {results['mse']}")
-# # # Load the dataset
-# # df = pd.read_csv("datasets/candy_production.csv", index_col=0, parse_dates=True)
-
-# # # Infer frequency and create exogenous variables (features)
-# # freq = infer_frequency(df)
-# # print(f"Inferred frequency: {freq}")
-# # exog = create_time_features(df_arg=df, freq=freq)
-
-# # # Evaluate the model
-# # results = evaluate_xgboost_and_random_forest(df=df, exog=exog, lag_value=15)
-# # print(f"The MSE of the model is: {results['mse']}")
+    new_row_mae.to_csv(
+        csv_mae, mode="a", header=False, index=False, lineterminator="\n"
+    )
+    new_row_mape.to_csv(
+        csv_mape, mode="a", header=False, index=False, lineterminator="\n"
+    )
+    new_row_mse.to_csv(
+        csv_mse, mode="a", header=False, index=False, lineterminator="\n"
+    )
+    new_row_rmse.to_csv(
+        csv_rmse, mode="a", header=False, index=False, lineterminator="\n"
+    )
